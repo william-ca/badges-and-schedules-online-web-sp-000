5d0ad7f170430ca3385fee24d444097695812601
Will Andre <wandre94@gmail.com>
2020-12-18 15:19:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,8 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  attendees.map { |name| badge_maker(name) }
+def batch_badge_creator(speakers)
+  speakers.map { |name| badge_maker(name) }
 end
+
+def assign_rooms()
